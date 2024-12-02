b2d56bed73ee8c4ee6275a21cd5cb27441934490
sanjanaganesh14 <sanjanaganesh1988@gmail.com>
2024-12-02 19:18:22 +0530

    Delete minitask1 directory

--- minitask1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/milisan/catkin_ws/src/minitask1
-# Build directory: /home/milisan/catkin_ws/build/minitask1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
